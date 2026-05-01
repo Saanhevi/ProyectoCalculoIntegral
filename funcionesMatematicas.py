c4ac25525724a1125f667ca8521338a58d35f55d
Samuel Herrera <saherrerav@unal.edu.co>
2026-05-01 12:32:01 -0500

    feat : Aregar funcion coseno y pi

--- funcionesMatematicas.py
+++ funcionesMatematicas.py
@@ -17,11 +17,11 @@ class fnMath :
         return resultado
 
     
-    def seno(x):
+    def seno(x : str):
         x = calcularNumero(x)
         resultado = 0
         
-        for n in range(100) : # n va desde 0 a 49
+        for n in range(100) : # n va desde 0 a 99
             termino = 1 /(fnMath.factorial(2*n+1))* (x)**(2*n+1)
             
             if n%2 == 0 : # Si n es par, entonces suma 
@@ -31,6 +31,20 @@ class fnMath :
                 
         return resultado
 
+    def coseno(x : str): 
+        x = calcularNumero(x)
+        resultado = 0
+        
+        for n in range(100) : # n va desde 0 a 99
+            termino = 1 /(fnMath.factorial(2*n))* (x)**(2*n)
+            
+            if n%2 == 0 : # Si n es par, entonces suma 
+                resultado += termino
+            else:         # Si n es impar, entonces resta
+                resultado -= termino
+                
+        return resultado
+        
     def euler():
         resultado = 0
         
@@ -40,4 +54,22 @@ class fnMath :
             
         return resultado
     
-print(fnMath.euler())
+
+    def pi(): 
+        # Basado en la serie hipergeometrica generalizada de Ramanujan 
+        resultadoSerie = 0
+        
+        for k in range (5):
+            numerador = (fnMath.factorial(6*k))*(13591409+545140134*k)
+            denominador = (fnMath.factorial(3*k))*(fnMath.factorial(k)**3)*(640320)**(3*k)
+            termino = numerador/denominador
+            
+            if k%2 == 0: 
+                resultadoSerie += termino
+            else: 
+                resultadoSerie -= termino
+        
+        return 1/(resultadoSerie*(12/(640320)**(1.5)))
+
+
+print(fnMath.coseno("pi/2 + 3"))
