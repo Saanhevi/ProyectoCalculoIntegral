3bb892ef95617583a28e6e4524b1872435617d48
Samuel Herrera <saherrerav@unal.edu.co>
2026-05-01 12:02:51 -0500

    feat : Agregar funcion seno

--- funcionesMatematicas.py
+++ funcionesMatematicas.py
@@ -1,11 +1,14 @@
+from identificadores import calcularNumero
+
 class fnMath :
     diccionarioFactorial = {}
     
-    def factorial (n):
+    def factorial(n):
         if n==1:
             return 1
         
-        resultado = fnMath.diccionarioFactorial.get(n,False)
+        # Se verifica si esta en el diccionario y no realizar varias recursiones 
+        resultado = fnMath.diccionarioFactorial.get(n,False) 
         
         if not (resultado) : # Si el resultado es falso entonces llama a la funcion
             resultado = n*fnMath.factorial(n-1)
@@ -13,3 +16,18 @@ class fnMath :
             
         return resultado
 
+    
+    def seno(x):
+        x = calcularNumero(x)
+        resultado = 0
+        
+        for n in range(100) : # n va desde 0 a 49
+            termino = 1 /(fnMath.factorial(2*n+1))* (x)**(2*n+1)
+            
+            if n%2 == 0 : # Si n es par, entonces suma 
+                resultado += termino
+            else:         # Si n es impar, entonces resta
+                resultado -= termino
+                
+        return resultado
+print(fnMath.seno("pi/2 + pi"))
